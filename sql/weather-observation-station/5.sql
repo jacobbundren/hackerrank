63379d14624c7c83ce796b28d200dd7171239986
Jacob Bundren <jacob.bundren@tpginc.com>
2023-08-08 16:35:40 -0500

    table and exercise 5

---
+++ sql/weather-observation-station/5.sql
@@ -0,0 +1,3 @@
+select top 1 city, len(city) from station where len(city) = (select max(len(city)) from station) order by city;
+select top 1 city, len(city) from station where len(city) = (select min(len(city)) from station) order by city;
+
---
+++ sql/weather-observation-station/create-table-station.sql
@@ -0,0 +1,9 @@
+create table station
+(
+    id     int,
+    city   varchar(21),
+    state  varchar(2),
+    lat_n  int,
+    long_w int
+)
+go
